90f590a7963d0944d3bb3e9e3ba0836c81b6a275
Corey Bruyere <corey@sundae.com>
2021-09-23 13:49:07 -0700

    Remove utils from stiches config temporarily

--- packages/core/src/stitches.config.ts
+++ packages/core/src/stitches.config.ts
@@ -1,5 +1,5 @@
 import { createStitches, globalCss } from '@stitches/react';
-import type * as Stitches from '@stitches/react';
+// import type * as Stitches from '@stitches/react';
 // @ts-ignore
 import { normalize } from 'normalize-stitches';
 
@@ -17,55 +17,55 @@ export const {
   config,
 } = createStitches({
   theme: defaultTheme,
-  utils: {
-    p: (value: Stitches.PropertyValue<'padding'>) => ({
-      padding: value,
-    }),
-    pt: (value: Stitches.PropertyValue<'paddingTop'>) => ({
-      paddingTop: value,
-    }),
-    pr: (value: Stitches.PropertyValue<'paddingRight'>) => ({
-      paddingRight: value,
-    }),
-    pb: (value: Stitches.PropertyValue<'paddingBottom'>) => ({
-      paddingBottom: value,
-    }),
-    pl: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
-      paddingLeft: value,
-    }),
-    px: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
-      paddingLeft: value,
-      paddingRight: value,
-    }),
-    py: (value: Stitches.PropertyValue<'paddingTop'>) => ({
-      paddingTop: value,
-      paddingBottom: value,
-    }),
+  // utils: {
+  //   p: (value: Stitches.PropertyValue<'padding'>) => ({
+  //     padding: value,
+  //   }),
+  //   pt: (value: Stitches.PropertyValue<'paddingTop'>) => ({
+  //     paddingTop: value,
+  //   }),
+  //   pr: (value: Stitches.PropertyValue<'paddingRight'>) => ({
+  //     paddingRight: value,
+  //   }),
+  //   pb: (value: Stitches.PropertyValue<'paddingBottom'>) => ({
+  //     paddingBottom: value,
+  //   }),
+  //   pl: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
+  //     paddingLeft: value,
+  //   }),
+  //   px: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
+  //     paddingLeft: value,
+  //     paddingRight: value,
+  //   }),
+  //   py: (value: Stitches.PropertyValue<'paddingTop'>) => ({
+  //     paddingTop: value,
+  //     paddingBottom: value,
+  //   }),
 
-    m: (value: Stitches.PropertyValue<'margin'>) => ({
-      margin: value,
-    }),
-    mt: (value: Stitches.PropertyValue<'marginTop'>) => ({
-      marginTop: value,
-    }),
-    mr: (value: Stitches.PropertyValue<'marginRight'>) => ({
-      marginRight: value,
-    }),
-    mb: (value: Stitches.PropertyValue<'marginBottom'>) => ({
-      marginBottom: value,
-    }),
-    ml: (value: Stitches.PropertyValue<'marginLeft'>) => ({
-      marginLeft: value,
-    }),
-    mx: (value: Stitches.PropertyValue<'marginLeft'>) => ({
-      marginLeft: value,
-      marginRight: value,
-    }),
-    my: (value: Stitches.PropertyValue<'marginTop'>) => ({
-      marginTop: value,
-      marginBottom: value,
-    }),
-  },
+  //   m: (value: Stitches.PropertyValue<'margin'>) => ({
+  //     margin: value,
+  //   }),
+  //   mt: (value: Stitches.PropertyValue<'marginTop'>) => ({
+  //     marginTop: value,
+  //   }),
+  //   mr: (value: Stitches.PropertyValue<'marginRight'>) => ({
+  //     marginRight: value,
+  //   }),
+  //   mb: (value: Stitches.PropertyValue<'marginBottom'>) => ({
+  //     marginBottom: value,
+  //   }),
+  //   ml: (value: Stitches.PropertyValue<'marginLeft'>) => ({
+  //     marginLeft: value,
+  //   }),
+  //   mx: (value: Stitches.PropertyValue<'marginLeft'>) => ({
+  //     marginLeft: value,
+  //     marginRight: value,
+  //   }),
+  //   my: (value: Stitches.PropertyValue<'marginTop'>) => ({
+  //     marginTop: value,
+  //     marginBottom: value,
+  //   }),
+  // },
 });
 
 // @ts-ignore
